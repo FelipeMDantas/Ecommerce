6cb4f7620d84fcfc3b75ca8ea0919f076a51bf88
FelipeMDantas <felipemdantas@terra.com.br>
2022-05-21 21:59:51 -0300

    Components further developed and communicating with each other.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Felipe&apos;s Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
